c3673d925f2f15bd8fbbc279e0455c4aa113d0d0
test <locally>
2020-03-07 17:25:04 +0800

    Initialized

---
+++ codility-lesson-002/src/main/java/personal/billson/codility/lesson002/App.java
@@ -0,0 +1,13 @@
+package personal.billson.codility.lesson002;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
